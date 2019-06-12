e2e43f3beb307d53fbd731b3ddf446e7c0f2dddd
kuraki <583378006@qq.com>
2019-06-12 10:32:55 +0800

    add one example

---
+++ study-for-leetcode/src/main/java/com/kuraki/algorithms/NoRepeatCharacter.java
@@ -0,0 +1,40 @@
+package com.kuraki.algorithms;
+
+import java.util.*;
+
+/**
+ * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+ */
+public class NoRepeatCharacter {
+
+    // 滑动窗口
+    public int lengthOfLongestSubstring(String s) {
+        int length = s.length();
+        Set<Character> set = new HashSet<>();
+        int ans = 0, i = 0, j = 0;
+        while (i < length && j < length) {
+            if (!set.contains(s.charAt(j))) {
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
+            } else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return ans;
+    }
+
+    // 优化的滑动窗口
+    public int lengOfLongestSubstring2(String s) {
+        int length = s.length(), ans = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < length; j++) {
+            if (map.containsKey(s.charAt(j))) {
+                i = Math.max(map.get(s.charAt(j)), i);
+            }
+            ans = Math.max(ans, j - i + 1);
+            map.put(s.charAt(j), j + 1);
+        }
+        return ans;
+    }
+}
