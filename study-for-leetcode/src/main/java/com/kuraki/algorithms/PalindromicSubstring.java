1005446610e6b055ff0457eebd7ae010eb8260bd
kuraki <583378006@qq.com>
2019-06-13 09:38:21 +0800

    add a example

---
+++ study-for-leetcode/src/main/java/com/kuraki/algorithms/PalindromicSubstring.java
@@ -0,0 +1,34 @@
+package com.kuraki.algorithms;
+
+public class PalindromicSubstring {
+
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() == 0) return "";
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            // 之所以调用过两次，是因为偶数和奇数的中心不一样
+            // 奇数的中心扩散
+            int len1 = expandAroundCenter(s, i, i);
+            // 偶数的中心扩散
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                // 获取回文的起始索引
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int expandAroundCenter(String s, int left, int right) {
+        int l = left, r = right;
+        // 以中心向两边扩散，一直相等，一直循环
+        while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+            l--;
+            r++;
+        }
+        // 返回这个回文的长度
+        return r - l - 1;
+    }
+}
