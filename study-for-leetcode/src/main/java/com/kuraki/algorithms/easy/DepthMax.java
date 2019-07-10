33d20bc792b8dac64e1a351536190ead16bed785
kuraki <583378006@qq.com>
2019-07-10 17:00:21 +0800

    update some detail

---
+++ study-for-leetcode/src/main/java/com/kuraki/algorithms/easy/DepthMax.java
@@ -0,0 +1,40 @@
+package com.kuraki.algorithms.easy;
+
+/**
+ * 给定一个二叉树，找出其最大深度。
+ * 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+ * 说明: 叶子节点是指没有子节点的节点。
+ */
+public class DepthMax {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        int depth = 1;
+        depth = countDepth(root.left, root.right, depth);
+        return depth;
+    }
+
+    private int countDepth(TreeNode left, TreeNode right, int depth) {
+        if (left == null && right == null) return depth;
+        depth++;
+        if (left == null) {
+            depth = countDepth(right.left, right.right, depth);
+        } else if (right == null) {
+            depth = countDepth(left.left, left.right, depth);
+        } else {
+            depth = Math.max(countDepth(right.left, right.right, depth), countDepth(left.left, left.right, depth));
+        }
+        return depth;
+    }
+
+    // 官方解答
+    public int maxDepth1(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            int left_height = maxDepth1(root.left);
+            int right_height = maxDepth1(root.right);
+            return Math.max(left_height, right_height) + 1;
+        }
+    }
+}
---
+++ study-for-leetcode/src/main/java/com/kuraki/algorithms/easy/LevelBottom.java
@@ -0,0 +1,62 @@
+package com.kuraki.algorithms.easy;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 给定一个二叉树，返回其节点值自底向上的层次遍历。
+ * （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
+ */
+public class LevelBottom {
+
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        helper(root, 0, res);
+        Collections.reverse(res);
+        return res;
+    }
+
+    private void helper(TreeNode root, int depth, List<List<Integer>> res) {
+        if (root == null)
+            return;
+        if (depth + 1 > res.size())
+            res.add(new ArrayList<>());
+        res.get(depth).add(root.val);
+        if (root.left != null) helper(root.left, depth + 1, res);
+        if (root.right != null) helper(root.right, depth + 1, res);
+    }
+
+    /*public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> lists = new ArrayList<>();
+        if (root == null) return lists;
+        TreeNode p = root;
+        List<Integer> list = new ArrayList<>();
+
+        if (root.left != null && root.right != null) {
+            queryLevelItem(root.left, list);
+            queryLevelItem(root.right, list);
+        } else {
+            queryLevelItem(root, list);
+        }
+
+
+    }
+
+    private List<Integer> queryLevelItem(TreeNode root, List<Integer> list) {
+        if (root.left == null && root.right == null) {
+            list.add(root.val);
+            return list;
+        }
+
+        if (root.right == null) {
+            list.add(root.left.val);
+        } else if (root.left == null) {
+            list.add(root.right.val);
+        } else {
+            list.add(root.left.val);
+            list.add(root.right.val);
+        }
+        return list;
+    }*/
+}
